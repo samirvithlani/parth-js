8edd4f8fac4814d1bfc4ac00305783eac5d95051
samirvithlani <samir.vithlani83955@gmail.com>
2023-12-30 09:09:57 +0530

    operaotr

---
+++ operators/arithmetic.js
@@ -0,0 +1,16 @@
+var a = 5
+var b = 3;
+
+//var x = a * b
+var x = a ** b
+console.log(x)
+
+
+var p = 10
+var q = 3
+
+//var r = p / q
+//var r = p / q
+//using Math.floor
+var r = Math.floor(p / q)
+console.log(r)
---
+++ operators/relational.js
@@ -0,0 +1,9 @@
+var x = 100
+var y = "100"
+
+
+//it will check the value only
+console.log(x == y)
+console.log(x === y)
+// console.log(x != y)
+// console.log(x !== y)
