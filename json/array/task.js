fb3f3ee2e3a85f9f50dfc5ce78b0a06eb65c2020
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-28 10:03:38 +0530

    task

---
+++ json/array/task.js
@@ -0,0 +1,43 @@
+var cars =[
+    {
+        "name": "audi",
+        models:[
+            {
+                name: "A4",
+                price: 35000,
+                colors: ["red", "blue", "black", "white", "silver"],
+                isAvailable: true,
+                gears: ["manual", "automatic"],
+                fuelType: ["petrol", "diesel", "electric"],
+                bodyType: ["sedan", "hatchback"],
+                engine: "2.0L",
+                seatingCapacity: 5,
+                transmission: "automatic",
+                mileage: 15,
+                fuelTankCapacity: 50,
+                airbags: 4,
+                abs: true,
+                childLock: true,
+                centralLock: true,
+                cruiseControl: true,
+                powerSteering: true,
+                powerWindows: true,
+
+            },
+            {
+                name: "A6",
+            }
+        ]
+    },
+    {
+        "name": "bmw",
+        models:[
+            {
+
+            },
+            {
+                
+            }
+        ]
+    }
+]
